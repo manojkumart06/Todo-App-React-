b52d7ee0fc68740988e24149d8b06a94e662050c
MANOJ KUMAR <manojkumar06t@gmail.com>
2024-05-20 10:22:54 +0530

    adding filter functionulity

--- todoapp/src/todoApp.js
+++ todoapp/src/todoApp.js
@@ -2,8 +2,11 @@ import { useState } from "react";
 import "./App.css";
 
 export default function TodoApp() {
+
+  //Managing the tasks using hooks
   const [tasks, setTasks] = useState([]);
   const [newTask, setNewTask] = useState("");
+  const [filter, setFilter] = useState("all");
 
   const addTask = () => {
     if (newTask.trim() !== "") {
@@ -22,7 +25,7 @@ export default function TodoApp() {
     );
   };
 
-  /*toggling the tasks*/
+  //toggling the tasks
   const toggleComplete = (id) => {
     setTasks(
       tasks.map((task) =>
@@ -31,9 +34,17 @@ export default function TodoApp() {
     );
   };
 
+  //Filtering the tasks based on condition
+  const filteredTasks = tasks.filter((task) => {
+    if (filter === "all") return true;
+    if (filter === "completed") return task.completed;
+    if (filter === "active") return !task.completed;
+    return true;
+  });
+
   return (
     <div className="App">
-      <h1>Todo App </h1>
+      <h1>Todo App</h1>
       <input
         type="text"
         value={newTask}
@@ -42,22 +53,47 @@ export default function TodoApp() {
         placeholder="Enter new task"
       />
       <button className="addButton" onClick={addTask}>
-        Add todo{" "}
+        Add todo
       </button>
+      <div>
+        <button
+          className={`filterButton ${filter === "all" ? "active" : ""}`}
+          onClick={() => setFilter("all")}
+        >
+          All
+        </button>
+        <button
+          className={`filterButton ${filter === "active" ? "active" : ""}`}
+          onClick={() => setFilter("active")}
+        >
+          Active
+        </button>
+        <button
+          className={`filterButton ${filter === "completed" ? "active" : ""}`}
+          onClick={() => setFilter("completed")}
+        >
+          Completed
+        </button>
+      </div>
       <ul>
-        {tasks.map((task) => (
+        {filteredTasks.map((task) => (
           <li key={task.id}>
             <input
               type="checkbox"
               checked={task.completed}
               onChange={() => toggleComplete(task.id)}
             />
-            {task.completed ? <del>{task.text}</del> : task.text}
-            <button className="deleteButton" onClick={() => deleteTask(task.id)}>Delete </button>
+            <div className="task-text">
+              {task.completed ? <del>{task.text}</del> : task.text}
+            </div>
+            <button className="deleteButton" onClick={() => deleteTask(task.id)}>
+              Delete
+            </button>
             <button
+              className="editButton"
               onClick={() => editTask(task.id, prompt("Edit task", task.text))}
             >
-              Edit{" "}
+              Edit
             </button>
           </li>
         ))}
